dd40bb19f433d9640cf07c91004ac13b78bf666e
yujingyi <yujingyi@qiyi.com>
2024-11-25 01:29:49 +0800

    remove useless file

--- ttkdescriber/combinationttk_test.go
+++ ttkdescriber/combinationttk_test.go
@@ -9,37 +9,37 @@ func TestAdd(t *testing.T) {
 
 func NewDamage() Damage {
 	damage := Damage{}
-	// damage.firerate = 667
-	// damage.health = 300
-
-	// damage.headRate = 15
-	// damage.neckRate = 5
-	// damage.upperTorsoRate = 30
-	// damage.lowerTorsoRate = 20
-	// damage.upperArmRate = 10
-	// damage.lowerArmRate = 5
-	// damage.upperLegRate = 10
-	// damage.lowerLegRate = 5
-
-	// damage.head = 43
-	// damage.neck = 41
-	// damage.upperTorso = 41
-	// damage.lowerTorso = 36
-	// damage.upperArm = 41
-	// damage.lowerArm = 41
-	// damage.upperLeg = 34
-	// damage.lowerLeg = 34
-
-	damage.firerate = 800
+	damage.firerate = 667
 	damage.health = 300
 
-	damage.headRate = 50
-	damage.neckRate = 50
-	// damage.upperTorsoRate = 25
-	// damage.lowerTorsoRate = 25
+	damage.headRate = 15
+	damage.neckRate = 5
+	damage.upperTorsoRate = 30
+	damage.lowerTorsoRate = 20
+	damage.upperArmRate = 10
+	damage.lowerArmRate = 5
+	damage.upperLegRate = 10
+	damage.lowerLegRate = 5
 
-	damage.head = 50
-	damage.neck = 40
+	damage.head = 43
+	damage.neck = 41
+	damage.upperTorso = 41
+	damage.lowerTorso = 36
+	damage.upperArm = 41
+	damage.lowerArm = 41
+	damage.upperLeg = 34
+	damage.lowerLeg = 34
+
+	// damage.firerate = 800
+	// damage.health = 300
+
+	// damage.headRate = 50
+	// damage.neckRate = 50
+	// // damage.upperTorsoRate = 25
+	// // damage.lowerTorsoRate = 25
+
+	// damage.head = 50
+	// damage.neck = 40
 	// damage.upperTorso = 100
 	// damage.lowerTorso = 50
 
